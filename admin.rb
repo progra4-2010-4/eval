0ffbe917d3bf4eedb60fff3492bfed4aa060f799
lfborjas <luis.borjas@escolarea.com>
2010-10-28 22:22:12 -0600

    un poco de css para el admin

--- admin.rb
+++ admin.rb
@@ -11,6 +11,7 @@ end
 
 class Admin < Sinatra::Base
     set :views, File.dirname(__FILE__) + '/admin_views'    
+    set :public, File.dirname(__FILE__) + '/public'
     configure do 
          DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/development.sqlite3"))
          DataMapper.auto_upgrade!
@@ -20,7 +21,7 @@ class Admin < Sinatra::Base
         @raw_messages = PushInfo.all
         @messages = @raw_messages.collect {|m| JSON.parse(m.message)}
         #p @messages
-        haml :dashboard
+        haml :dashboard, :layout=>false
     end
     
     post '/turnin' do 
